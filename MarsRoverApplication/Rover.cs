dd7d99f6f44baa98e1ff21b6b83738233895982c
Rita Bakos <rita.bakos@tba.group>
2025-04-02 13:56:38 +0300

    Add Direction property to Rover class

--- MarsRoverApplication/Rover.cs
+++ MarsRoverApplication/Rover.cs
@@ -2,16 +2,18 @@
 {
     public class Rover
     {
-        public Rover(int xCoordinate, int yCoordinate)
+        public Rover(int xCoordinate, int yCoordinate, string direction)
         {
             CurrentCoordinate = new()
             {
                 X = xCoordinate,
                 Y = yCoordinate
             };
+            Direction = direction;
         }
 
         public Coordinate CurrentCoordinate { get; set; }
+        public string Direction { get; set; } = "N";
 
         public class Coordinate 
          {
--- MarsRoverTests/RoverTests.cs
+++ MarsRoverTests/RoverTests.cs
@@ -13,10 +13,21 @@ namespace MarsRoverTests
         [TestCase(1, 1)]
         public void InitialiseMarsRoverCoordinates(int initalXCoordinate, int initalYCoordinate)
         {
-            Rover rover = new Rover(xCoordinate: initalXCoordinate, yCoordinate: initalYCoordinate);
+            Rover rover = new Rover(xCoordinate: initalXCoordinate, yCoordinate: initalYCoordinate, "N");
 
             Assert.That(rover.CurrentCoordinate.X, Is.EqualTo(initalXCoordinate));
             Assert.That(rover.CurrentCoordinate.Y, Is.EqualTo(initalYCoordinate));
+        }
+
+        [TestCase("N")]
+        [TestCase("S")]
+        [TestCase("E")]
+        [TestCase("W")]
+        public void InitialiseMarsRoverDirection(string direction)
+        {
+            Rover rover = new Rover(0, 0, direction);
+
+            Assert.That(rover.Direction, Is.EqualTo(direction));
         }
     }
 }
